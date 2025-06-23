ca0654997a738a6694cf0dd59266e291fbff7dc3
Csd123 <1962777083@qq.com>
2025-06-23 21:31:25 +0800

    update data info

--- internvl_chat/run_examples.sh
+++ internvl_chat/run_examples.sh
@@ -1,3 +1,5 @@
 conda activate internvl_env
 python simple_inference_demo.py     --input /mnt/chensenda/codes/VLN/InternVL/internvl_chat/examples/image1.jpg    --question "Describe what you see in this image"
 python simple_inference_demo.py     --input /mnt/chengchangxu/data/VSI-Bench/arkitscenes/41069025.mp4    --question "<video>\nHow many table(s) are in this room?"
+
+python simple_inference_demo.py     --input /mnt/chengchangxu/data/VSI-Bench/arkitscenes/41254386.mp4    --question "What is the length of the longest dimension (length, width, or height) of the toilet, measured in centimeters?"
